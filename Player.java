ea508a0e7da6890d16fbfc4948084bdd31548fe2
Yatharth <52836348+yatharth-16@users.noreply.github.com>
2020-08-08 23:05:48 +0530

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,41 @@
+
+public class Player {
+    
+	
+	private String name;
+	private char symbol;
+	
+	public Player(String name, char symbol)
+	{
+		setName(name);
+		
+		setSymbol(symbol);
+	}
+	
+	
+	public void setName(String name)
+	{
+		if(!name.isEmpty())
+		{
+			this.name=name;
+		}
+	}
+	
+     public String getName()
+     {
+    	 return name;
+     }
+     
+     public void setSymbol(char symbol)
+     {
+    	 if(symbol!='\0')
+    	 {
+    		 this.symbol=symbol;
+    	 }
+     }
+     public char getSymbol()
+     {
+    	 return symbol;
+     }
+     
+} 
